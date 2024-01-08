f0ac709f6b1af754a89f9d3c64f8ba58876c6f58
Piyush Yadav <piyushya012@gmail.com>
2024-01-08 21:31:53 +0530

    cleanup

--- index.js
+++ index.js
@@ -13,15 +13,12 @@ form.addEventListener("submit", (event) => {
 // send form data to server and show loader
 async function sendData() {
     const formData = new FormData(form);
-    // formData.append('source_csv_file', fileInput.files[0]);
-    // formData.append('class_name', class_name);
-    // formData.append('semester_number', semester);
   try {
     displayLoader();
     const response = await fetch("http://127.0.0.1:5000/fetch_results", {
       method: "POST",
-    //   mode: "no-cors",
-      // Set the FormData instance as the request body
+        // mode: "no-cors",
+        // Set the FormData instance as the request body
       body: formData,
     });
     console.log(response.status);
@@ -36,7 +33,7 @@ async function sendData() {
   }
 }
 
-// download manually
+// download manually or remove this
 dowload.addEventListener("click", () => {
 
     // send download request to server
